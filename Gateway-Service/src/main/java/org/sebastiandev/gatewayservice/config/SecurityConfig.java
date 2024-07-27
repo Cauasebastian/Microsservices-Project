da30e00be6c2fc728bff14894d039f5053bcd679
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-07-27 01:02:33 -0300

    [FIX] Container inicialization errors fixed

--- Gateway-Service/src/main/java/org/sebastiandev/gatewayservice/config/SecurityConfig.java
+++ Gateway-Service/src/main/java/org/sebastiandev/gatewayservice/config/SecurityConfig.java
@@ -5,39 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    /**
-     * This method is used to configure the security of the application.
-     * @param http
-     * @return
-     */
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/eureka/**").permitAll()
-                        .anyExchange().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2
-                .jwt(Customizer.withDefaults()));
-        return http.build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+        return serverHttpSecurity.build();
     }
-    /**
-     * This method is used to configure the JWT decoder
-     * @return
-     */
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        // Replace "issuer-uri" with your actual issuer URI
-        String issuerUri = "http://localhost:8181/realms/spring-boot-microservices-realm";
-        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuerUri);
-    }
-
 }
